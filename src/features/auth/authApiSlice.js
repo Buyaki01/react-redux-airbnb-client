912c2d24df7f29e42449c2279ccf756af22dbcbb
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-03 14:37:28 +0300

    Add authSlice and authApiSlice

---
+++ src/features/auth/authApiSlice.js
@@ -0,0 +1,18 @@
+import { apiSlice } from "../../app/api/apiSlice"
+import { logOut } from "./authSlice"
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: builder => ({
+    login: builder.mutation({
+      query: credentials => ({
+        url: '/auth',
+        method: 'POST',
+        body: { ...credentials }
+      })
+    }),
+  })
+})
+
+export const {
+  useLoginMutation
+} = authApiSlice 
