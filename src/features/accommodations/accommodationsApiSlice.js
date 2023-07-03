f4bd72cc187869f141d0f2bcdc250a4870fe26e3
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-03 09:00:44 +0300

    Add accommodationsApiSlice with getAccommodations query

---
+++ src/features/accommodations/accommodationsApiSlice.js
@@ -0,0 +1,54 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit"
+import { apiSlice } from "../../app/api/apiSlice"
+
+const accommodationsAdapter = createEntityAdapter({})
+
+const initialState = usersAdapter.getInitialState()
+
+export const accommodationsApiSlice = apiSlice.injectEndpoints({
+  endpoints: builder => ({
+    getAccommodations: builder.query({
+      query: () => ({
+        url: '/accommodations',
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError
+        }
+      }),
+      keepUnusedDataFor: 5,
+      transformResponse: responseData => { //responseData is from the query
+        const loadedAccommodations = responseData.map(accommodation => {
+          accommodation.id = accommodation._id
+          return accommodation
+        });
+        return accommodationsAdapter.setAll(initialState, loadedAccommodations)
+      }, 
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: 'Accommodation', id: 'LIST'},
+            ...result.ids.map(id => ({ type: 'Accommodation', id }))
+          ]
+        } //If the result does not contain any ids, it means that the query or mutation did not retrieve or modify any specific items
+        else return [{ type: 'Accommodation', id: 'LIST'}] //This tag represents the entire list or collection of Accommodation items, indicating that the cache should be updated or invalidated for the entire list
+
+      }
+    }),
+  }),
+})
+
+export const {
+  useGetAccommodationsQuery,
+} = accommodationsApiSlice
+
+export const selectAccommodationsResult = accommodationsApiSlice.endpoints.getAccommodations.select()
+
+export const selectAccommodationsData = createSelector(
+  selectAccommodationsResult,
+  accommodationsResult => accommodationsResult.data
+)
+
+export const {
+  selectAll: selectAllAccommodations,
+  selectById: selectAccommodationById,
+  selectIds: selectAccommodationIds
+} = accommodationsAdapter.getSelectors(state => selectAccommodationsData(state) ?? initialState)
