13ccf655d027455398aa99152564844e4607b4e2
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-03 14:05:38 +0300

    Add getAllAccommodationsForOwner query and update MyAccommodations component

--- src/components/Accommodations/MyAccommodationsPage.js
+++ src/components/Accommodations/MyAccommodationsPage.js
@@ -1,40 +1,90 @@
 import { Link } from "react-router-dom"
+import { useGetAllAccommodationsForOwnerQuery } from "../../features/accommodations/accommodationsApiSlice"
 
 const AccommodationsPage = () => {
-  return (
-    <div>
-      <div className="text-center mt-3">
-        <Link 
-          className="inline-flex bg-primary text-white py-2 px-6 rounded-full" 
-          to={'/accommodations/new'}
-        > 
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-          </svg>
-          Add new Accommodation 
-        </Link>
-      </div>
+  const { data: accommodations, isLoading, isSuccess, isError, error } = useGetAllAccommodationsForOwnerQuery()
 
-      <div className="mt-4">
-        {/* {places.length > 0 && places.map(place => ( */}
-          <Link 
-            // to={`/accommodations/edit/${place._id}`}
-            className="flex cursor-pointer gap-4 bg-gray-100 p-4 mb-4 rounded-2xl" 
+  let content
+
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>
+  }
+
+  if (isSuccess) {
+    const { ids, entities } = accommodations
+    console.log(ids)
+
+    content = (
+      <div>
+        <div className="text-center mt-3">
+          <Link
+            className="inline-flex bg-primary text-white py-2 px-6 rounded-full"
+            to={"/accommodations/new"}
           >
-            <div className="flex w-32 h-32 bg-gray-300 grow flex-shrink-0">
-              {/* {place.photos.length > 0 && ( */}
-                <img className="object-cover" src="" alt="" />
-              {/* )}  */}
-            </div>
-            <div className="flex-grow-0">
-              <h2 className="text-xl">title</h2>
-              <p className="text-sm mt-2">description</p>
-            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            Add new Accommodation
           </Link>
-        {/* )) } */}
+        </div>
+
+        <div className="mt-4">
+          {ids.map((id) => {
+            const accommodation = entities[id]
+            return (
+              <div key={id}>
+                <div className="text-center mt-3">
+                  <Link 
+                    className="inline-flex bg-primary text-white py-2 px-6 rounded-full" 
+                    to={'/accommodations/new'}
+                  > 
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    Add new Accommodation 
+                  </Link>
+                </div>
+
+                <div className="mt-4">
+                  <Link 
+                    to={`/accommodations/edit/${accommodation.id}`}
+                    className="flex cursor-pointer gap-4 bg-gray-100 p-4 mb-4 rounded-2xl" 
+                  >
+                    <div className="flex w-32 h-32 bg-gray-300 grow flex-shrink-0">
+                      {/* {place.photos.length > 0 && ( */}
+                        <img className="object-cover" src="" alt={accommodation.title} />
+                      {/* )}  */}
+                    </div>
+                    <div className="flex-grow-0">
+                      <h2 className="text-xl">{accommodation.title}</h2>
+                      <p className="text-sm mt-2">{accommodation.description}</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            )
+          })}  
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  return content
 }
 
 export default AccommodationsPage
--- src/features/accommodations/accommodationsApiSlice.js
+++ src/features/accommodations/accommodationsApiSlice.js
@@ -33,11 +33,38 @@ export const accommodationsApiSlice = apiSlice.injectEndpoints({
 
       }
     }),
+    getAllAccommodationsForOwner: builder.query({
+      query: () => ({
+        url: '/accommodations/myaccommodations',
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError
+        }
+      }),
+      keepUnusedDataFor: 60,
+      transformResponse: responseData => { //responseData is from the query
+        const loadedAccommodations = responseData.map(accommodation => {
+          accommodation.id = accommodation._id
+          return accommodation
+        });
+        return accommodationsAdapter.setAll(initialState, loadedAccommodations)
+      }, 
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: 'Accommodation', id: 'LIST'},
+            ...result.ids.map(id => ({ type: 'Accommodation', id }))
+          ]
+        } //If the result does not contain any ids, it means that the query or mutation did not retrieve or modify any specific items
+        else return [{ type: 'Accommodation', id: 'LIST'}] //This tag represents the entire list or collection of Accommodation items, indicating that the cache should be updated or invalidated for the entire list
+
+      }
+    }),
   }),
 })
 
 export const {
   useGetAccommodationsQuery,
+  useGetAllAccommodationsForOwnerQuery,
 } = accommodationsApiSlice
 
 export const selectAccommodationsResult = accommodationsApiSlice.endpoints.getAccommodations.select()
