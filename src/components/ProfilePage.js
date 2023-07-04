dc1971f822075fba1b223e8e51416e318f61d619
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-04 10:47:48 +0300

    Add headers in baseQuery apiSlice

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,19 +1,32 @@
+import { useGetProfileQuery, selectProfileData } from "../features/profile/profileApiSlice"
 import { useSelector } from "react-redux"
-import { selectCurrentEmail } from "../features/profile/profileSlice"
 
 const ProfilePage = () => {
-  // const username = useSelector(selectCurrentUsername)
-  const email = useSelector(selectCurrentEmail)
-  console.log(email)
-
-  return (
-    <div> 
-      <div className="text-center">
-        <h5>Logged in as {email} </h5> 
-        <button className="primary max-w-sm mt-3">Logout</button>
+  const { data: profile, isLoading, isSuccess, isError, error } = useGetProfileQuery()
+  const profileData = useSelector(selectProfileData)
+  
+  let content
+  
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+  
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>
+  }
+ 
+  if (isSuccess) {
+    content (
+      <div> 
+        <div className="text-center">
+         <h5>Logged in as {profileData.email} </h5>
+          <button className="primary max-w-sm mt-3">Logout</button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  return content
 }
 
-export default ProfilePage
+export default ProfilePage;
--- src/features/profile/profileApiSlice.js
+++ src/features/profile/profileApiSlice.js
@@ -1,9 +1,53 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit"
 import { apiSlice } from "../../app/api/apiSlice"
+
+const profileAdapter = createEntityAdapter({})
+
+const initialState = profileAdapter.getInitialState()
 
 export const profileApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
-    fetchProfile: builder.query({
-      query: () => '/profile',
+    getProfile: builder.query({
+      query: () => ({
+        url: '/profile',
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError
+        }
+      }),
+      transformResponse: responseData => { //responseData is from the query
+        const loadedProfile = responseData.map(profile => {
+          profile.id = profile._id
+          return profile
+        });
+        return profileAdapter.setAll(initialState, loadedProfile)
+      },
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: 'User', id: 'LIST'},
+            ...result.ids.map(id => ({ type: 'User', id }))
+          ]
+        }
+        else return [{ type: 'User', id: 'LIST'}] 
+      }
     }),
   })
 })
+
+
+export const {
+  useGetProfileQuery
+} = profileApiSlice
+
+export const selectProfileResult = profileApiSlice.endpoints.getProfile.select()
+
+export const selectProfileData = createSelector(
+  selectProfileResult,
+  profileResult => profileResult.data
+)
+
+export const {
+  selectAll: selectAllProfile,
+  selectById: selectProfileById,
+  selectIds: selectProfileIds
+} = profileAdapter.getSelectors(state => selectProfileData(state) ?? initialState)
