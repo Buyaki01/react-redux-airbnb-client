d586c81d5b100a27d280d9699e9979d236b3578b
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-05 09:36:03 +0300

    Rename AccommodationsPage and IndexPage

--- src/components/Accommodations/AccommodationsPage.js
+++ src/components/Accommodations/AccommodationsPage.js
@@ -1,38 +1,18 @@
 import { Link } from "react-router-dom"
 
 const AccommodationsPage = () => {
-  return (
-    <div>
-      <div className="text-center mt-3">
-        <Link 
-          className="inline-flex bg-primary text-white py-2 px-6 rounded-full" 
-          to={'/accommodations/new'}
-        > 
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-          </svg>
-          Add new Accommodation 
-        </Link>
-      </div>
-
-      <div className="mt-4">
-        {/* {places.length > 0 && places.map(place => ( */}
-          <Link 
-            // to={`/accommodations/edit/${place._id}`}
-            className="flex cursor-pointer gap-4 bg-gray-100 p-4 mb-4 rounded-2xl" 
-          >
-            <div className="flex w-32 h-32 bg-gray-300 grow flex-shrink-0">
-              {/* {place.photos.length > 0 && ( */}
-                <img className="object-cover" src="" alt="" />
-              {/* )}  */}
-            </div>
-            <div className="flex-grow-0">
-              <h2 className="text-xl">title</h2>
-              <p className="text-sm mt-2">description</p>
-            </div>
-          </Link>
-        {/* )) } */}
-      </div>
+  return(
+    <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+      <Link>
+        <div className="bg-gray-500 mb-2 rounded-2xl flex"> 
+          <img className="rounded-2xl object-cover aspect-square" src="" alt="" />
+        </div>
+        <h2 className="font-semibold">address</h2>
+        <h3 className="text-gray-700" >title</h3>
+        <div className="mt-1">
+          <span className="font-semibold">price</span> per night
+        </div>
+      </Link>
     </div>
   )
 }
---
+++ src/components/Accommodations/MyAccommodationsPage.js
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom"
+
+const MyAccommodationsPage = () => {
+  return (
+    <div>
+      <div className="text-center mt-3">
+        <Link 
+          className="inline-flex bg-primary text-white py-2 px-6 rounded-full" 
+          to={'/accommodations/new'}
+        > 
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+          Add new Accommodation 
+        </Link>
+      </div>
+
+      <div className="mt-4">
+        {/* {places.length > 0 && places.map(place => ( */}
+          <Link 
+            // to={`/accommodations/edit/${place._id}`}
+            className="flex cursor-pointer gap-4 bg-gray-100 p-4 mb-4 rounded-2xl" 
+          >
+            <div className="flex w-32 h-32 bg-gray-300 grow flex-shrink-0">
+              {/* {place.photos.length > 0 && ( */}
+                <img className="object-cover" src="" alt="" />
+              {/* )}  */}
+            </div>
+            <div className="flex-grow-0">
+              <h2 className="text-xl">title</h2>
+              <p className="text-sm mt-2">description</p>
+            </div>
+          </Link>
+        {/* )) } */}
+      </div>
+    </div>
+  )
+}
+
+export default MyAccommodationsPage
