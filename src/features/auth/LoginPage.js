dea515d83e6abadfebd21fde86a754b1f51acc59
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-07 15:46:53 +0300

    Add functionality to LoginPage

--- src/features/auth/LoginPage.js
+++ src/features/auth/LoginPage.js
@@ -1,18 +1,77 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useRef, useState, useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { setCredentials } from "./authSlice"
+import { useLoginMutation } from "./authApiSlice"
 
 const LoginPage = () => {
+  const emailRef = useRef()
+  const errRef = useRef()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errMsg, setErrMsg] = useState('')
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const [login, { isLoading }] = useLoginMutation()
+
+  useEffect(() => {
+    emailRef.current.focus()
+  }, [])
+
+  useEffect(() => {
+    setErrMsg('')
+  }, [email, password])
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const { accessToken } = await login({ email, password }).unwrap()
+      dispatch(setCredentials({ accessToken }))
+      setEmail('')
+      setPassword('')
+      navigate('/')
+    } catch (err) {
+      if (!err.status) {
+        setErrMsg('No Server Response')
+      } else if (err.status === 400) {
+        setErrMsg('Missing Email or Password')
+      } else if (err.status === 401) {
+        setErrMsg('Unauthorized')
+      } else {
+        setErrMsg(err.data?.message)
+      }
+      errRef.current.focus()
+    }
+  }
+
+  const errClass = errMsg ? "errmsg" : "offscreen"
+
+  if (isLoading) return <p>Loading...</p>
+
   return(
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md max-auto">
+
+        <p ref={errRef} className={errClass} aria-live="assertive">{errMsg}</p>
+
+        <form className="max-w-md max-auto" onSubmit={handleLoginSubmit}>
           <input
             type="email"
             placeholder="your@email.com"
+            ref={emailRef}
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            autoComplete="off"
+            required
           />
           <input
             type="password"
             placeholder="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
           <button className="primary mt-2"> Login </button>
           <div className="text-center mt-2 text-gray-500">
