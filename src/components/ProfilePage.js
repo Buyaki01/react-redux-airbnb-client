e17d5a694fc4708a003aada6799a91a992a062ee
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-07 16:17:29 +0300

    Add functionality for logging out

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,9 +1,36 @@
+import { useNavigate } from "react-router-dom"
+import { useSendLogoutMutation } from "../features/auth/authApiSlice"
+import { useEffect } from "react"
+
 const ProfilePage = () => {
+  const navigate = useNavigate()
+
+  const [sendLogout, {
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  }] = useSendLogoutMutation()
+
+  useEffect(() => {
+    if (isSuccess) navigate('/')
+  }, [isSuccess, navigate])
+
+  if (isLoading) return <p>Logging Out...</p>
+
+  if (isError) return <p>Error: {error.data?.message}</p>
+
   return (
     <div> 
       <div className="text-center">
         <h5>Logged in as </h5> 
-        <button className="primary max-w-sm mt-3">Logout</button>
+        <button 
+          className="primary max-w-sm mt-3" 
+          title="Logout"
+          onClick={sendLogout}
+        >
+          Logout
+        </button>
       </div>
     </div>
   )
