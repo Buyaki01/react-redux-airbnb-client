19a9ce291341daa432012ed75cd547ff96fcecb7
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-10 12:46:21 +0300

    Add username after the user avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
 
 const Header = () => {
+
+  const { username } = useAuth()
+
   return(
     <header className='flex justify-between'>
       <Link 
@@ -27,7 +31,7 @@ const Header = () => {
       </div>
         
       <Link 
-        // to={user?'/account/profile':'/login'}
+        to={username ? '/profile' : '/login'}
         className='flex flex-center gap-2 border border-gray-300 rounded-full py-2 px-4'
       >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
@@ -39,6 +43,13 @@ const Header = () => {
             <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
           </svg>
         </div>
+
+        {!!username && (
+          <div>
+            {username}
+          </div>
+        )}
+
       </Link>
     </header>
   )
