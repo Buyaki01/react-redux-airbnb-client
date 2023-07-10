c0a2fa43224353680431a6f39990e0f07d3567a6
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-10 10:26:42 +0300

    Add functionality to PersistLogin component

--- src/features/auth/PersistLogin.js
+++ src/features/auth/PersistLogin.js
@@ -19,6 +19,57 @@ const PersistLogin = () => {
     isError,
     error
   }] = useRefreshMutation()
+
+  useEffect(() => {
+
+    if (effectRan.current === true || process.env.NODE_ENV !== 'development') {
+
+      const verifyRefreshToken = async () => {
+        console.log('verifying refresh token')
+        try {
+          const response = await refresh()
+
+          // const { accessToken } = response.data
+          
+          setTrueSuccess(true) //Giving time for the credentials above to be set
+        } catch (err) {
+          console.error(err)
+        }
+      }
+
+      if (!token && persist) verifyRefreshToken()
+    }
+
+    return () => effectRan.current = true
+
+    // eslint-disable-next-line
+  }, [])
+
+  let content
+  if (!persist) { // persist: no
+    console.log('no persist')
+    content = <Outlet />
+  } else if (isLoading) { //persist: yes, token: no
+    console.log('loading')
+    content = <p>Loading...</p>
+  } else if (isError) { //persist: yes, token: no
+    console.log('error')
+    content = (
+      <p className="errmsg">
+        {error.data?.message}
+        <Link to="/login">Please login again</Link>
+      </p>
+    )
+  } else if (isSuccess && trueSuccess) { //persist: yes, token: yes
+    console.log('success')
+    content = <Outlet />
+  } else if (token && isUninitialized) { //persist: yes, token: yes
+    console.log('token and uninit')
+    console.log(isUninitialized)
+    content = <Outlet />
+  }
+
+  return content
 }
 
 export default PersistLogin
