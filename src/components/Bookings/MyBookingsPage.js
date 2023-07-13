caab08fd520b42ac2e69388416e7bd172fcef5ec
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-13 08:23:33 +0300

    Pass data from the store to mybookings

--- src/components/Bookings/MyBookingsPage.js
+++ src/components/Bookings/MyBookingsPage.js
@@ -2,32 +2,24 @@ import { Link } from "react-router-dom"
 import BookingDates from "./BookingDates"
 import useAuth from "../../hooks/useAuth"
 import { useSelector } from "react-redux"
-import { selectAllBookings, useGetBookingsQuery } from "../../features/Bookings/bookingsApiSlice"
+import { selectAllBookings } from "../../features/Bookings/bookingsApiSlice"
 
 const BookingsPage = () => {
   
   const { id: userId } = useAuth()
-  
-  const { 
-    data: bookings, 
-    isLoading, 
-    isSuccess, 
-    isError, 
-    error } = useGetBookingsQuery('bookingsList', {
-      pollingInterval: 15000, 
-      refetchOnFocus: true,
-      refetchOnMountOrArgChange: true
-    })
-  
-  console.log(bookings)
+
+  const bookings = useSelector(selectAllBookings)
+
+  const ownersBookings = bookings.filter((booking) => booking.userId === userId)
 
   return(
     <div>
       <div>
-        {/* {ownersBookings.length > 0 &&
-          ownersBookings.map((booking) => ( */}
+        {ownersBookings.length > 0 &&
+          ownersBookings.map((booking) => (
             <Link
-              // to={`/booking/${booking.id}`}
+              key={booking.id}
+              to={`/booking/${booking.id}`}
               className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden mt-3"
             >
               <div className="w-48">
@@ -40,17 +32,19 @@ const BookingsPage = () => {
               <div className="py-3 pr-3 grow">
                 <h2 className="text-xl">booking.accomodationId.title</h2>
                 <div className="text-xl">
-                  {/* <BookingDates booking={booking} /> */}
+
+                  <BookingDates booking={booking} />
+                  
                   <div>
                     <span className="text-2xl">
-                      Total price: $booking.price
+                      Total price: ${booking.price}
                     </span>
                   </div>
                 </div>
               </div>
             </Link>
-          {/* )) 
-        } */}
+          )) 
+        }
       </div>
     </div>
   )
