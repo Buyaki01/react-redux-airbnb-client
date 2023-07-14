593278e111afbe3a4a2aad0e520db86e6a65d0d0
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-14 18:41:58 +0300

    Getting null data

--- src/components/Accommodations/PhotosSection.js
+++ src/components/Accommodations/PhotosSection.js
@@ -20,20 +20,28 @@ const PhotosSection = ({addPhoto, setAddPhoto}) => {
   }
 
   const uploadPhoto = async (e) => {
-    const file = e.target.files
+    const files = e.target.files
+    
     const formData = new FormData()
-    for (let i = 0; i < file.length; i++) {
-      formData.append('file', file[i])
+    console.log(formData)
+
+    for (let i = 0; i < files.length; i++) {
+      formData.append('photos', files[i])
     }
     
     try {
-      const { data: filenames } = await addPhotoFromDevice({ formData })
-      setAddPhoto(prev => [...prev, ...filenames])
+      const uploadedPhoto = await addPhotoFromDevice({ formData })
+      // const { data: filenames } = await addPhotoFromDevice({ formData }) //{data: null}
+
+      console.log(uploadedPhoto)
+      
+      // setAddPhoto(prev => [...prev, ...filenames])
+
     } catch (error) {
       // Handle the error if the upload fails
       console.log('Failed to upload photo:', error)
     }
-    
+
   }
 
   // const deletePhoto = (e, filename) => {
@@ -84,7 +92,7 @@ const PhotosSection = ({addPhoto, setAddPhoto}) => {
           )) }
 
         <label className="h-32 cursor-pointer flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
-          <input type="file" className="hidden" multiple onChange={uploadPhoto} />
+          <input type="file" className="hidden" name="photos" multiple onChange={uploadPhoto} />
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
           </svg>
