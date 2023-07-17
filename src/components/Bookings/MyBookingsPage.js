449065044806504ea2701e7132ed641c0f0725b7
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-17 18:56:11 +0300

    Get accommodation that matches booking.accommodationId

--- src/components/Bookings/MyBookingsPage.js
+++ src/components/Bookings/MyBookingsPage.js
@@ -3,48 +3,56 @@ import BookingDates from "./BookingDates"
 import useAuth from "../../hooks/useAuth"
 import { useSelector } from "react-redux"
 import { selectAllBookings } from "../../features/Bookings/bookingsApiSlice"
+import { selectAllAccommodations } from "../../features/accommodations/accommodationsApiSlice"
 
 const MyBookingsPage = () => {
   
   const { id: userId } = useAuth()
 
+  const accommodations = useSelector(selectAllAccommodations)
+
   const bookings = useSelector(selectAllBookings)
 
   const ownersBookings = bookings.filter((booking) => booking.userId === userId)
 
-  return(
+  return (
     <div>
       <div>
         {ownersBookings.length > 0 &&
-          ownersBookings.map((booking) => (
-            <Link
-              key={booking.id}
-              to={`/booking/${booking.id}`}
-              className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden mt-3"
-            >
-              <div className="w-48">
-                <img
-                  className="object-cover"
-                  // src={`http://localhost:5000/images/${booking.accommodationId.photos[0]}`}
-                  alt=""
-                />
-              </div>
-              <div className="py-3 pr-3 grow">
-                {/* <h2 className="text-xl">{booking.accommodationId.title}</h2> */}
-                <div className="text-xl">
+          ownersBookings.map((booking) => {
+            const matchingAccommodation = accommodations.find(
+              (accommodation) => accommodation.id === booking.accommodationId
+            )
 
-                  <BookingDates booking={booking} />
-                  
-                  <div>
-                    <span className="text-2xl">
-                      Total price: ${booking.price}
-                    </span>
+            return (
+              <Link
+                key={booking.id}
+                to={`/booking/${booking.id}`}
+                className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden mt-3"
+              >
+                <div className="w-48">
+                  <img
+                    className="object-cover"
+                    src={`http://localhost:5000/images/${matchingAccommodation.photos[0]}`}
+                    alt=""
+                  />
+                </div>
+                <div className="py-3 pr-3 grow">
+                  <div className="text-xl">
+                    <h2 className="text-xl">{matchingAccommodation.title}</h2>
+
+                    <BookingDates booking={booking} />
+
+                    <div>
+                      <span className="text-2xl">
+                        Total price: ${booking.price}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          )) 
-        }
+              </Link>
+            )
+          })}
       </div>
     </div>
   )
